70ccb10b3323973bf4a5ae209565d0109cfb31a4
isac <isacsk@kth.se>
2022-09-24 21:05:08 +0200

    Added a login rest api.

--- api/serializers.py
+++ api/serializers.py
@@ -1,10 +1,17 @@
 from rest_framework import serializers
 from .models import DefaultUser
+from django.contrib.auth.hashers import make_password
 
 
 class DefaultUserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = DefaultUser
-        fields = '__all__'
+        fields = ['username', 'email', 'password']
         extra_kwargs = {'password': {'write_only': True}}
+
+    def validate_password(self, data):
+        return make_password(data)
+
+
+
--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth import login
 from .models import DefaultUser, Friendship, FriendRequest, Message
-from rest_framework import status
+from rest_framework import status, exceptions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import DefaultUserSerializer
@@ -16,6 +17,25 @@ class Register(APIView):
             return Response(status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class Login(APIView):
+
+    def post(self, request):
+        if not (request.data['username'] and request.data['password']):
+            return Response({'Authentication failed': 'Insufficient credentials.'}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            username = request.data['username']
+            password = request.data['password']
+            print(username, password)
+            user = DefaultUser.objects.filter(username=username).first()
+            if not (user and user.check_password(raw_password=password)):
+                return Response({'Authentication failed': 'Invalid credentials.'}, status=status.HTTP_404_NOT_FOUND)
+            else:
+                login(request, user)
+                return Response({'Authentication succeeded': 'Logged in.'}, status=status.HTTP_200_OK)
+
+
 
 
 
