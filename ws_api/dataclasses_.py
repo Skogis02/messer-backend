22463c72074946ce1cd596ea2205a956c59ffa72
isac <isacsk@kth.se>
2022-12-08 19:44:35 +0100

    Created new structure for the ws API. Incomplete.

---
+++ ws_api/dataclasses_.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass, field
+import json
+
+@dataclass
+class Error:
+    error: str
+    code: int
+    content: str = ''
+
+    def as_dict(self):
+        return {
+            'error': self.error,
+            'content': self.content,
+            'code': self.code
+        }
+
+@dataclass
+class Response:
+    endpoint: str
+    id: int
+    content: dict = field(default_factory=dict)
+    errors: list[Error] = field(default_factory=list)
+
+    def as_dict(self):
+        return {
+            'endpoint': self.endpoint,
+            'id': self.id,
+            'content': self.content,
+            'errors': [error.as_dict() for error in self.errors]
+        }
