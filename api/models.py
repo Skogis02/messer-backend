3ab94a0017cb81b4f863e47c7ec9d8a85369a207
isac <isacsk@kth.se>
2022-12-16 15:25:16 +0100

    Merging database-manager branch

--- api/models.py
+++ api/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.dispatch import receiver
-
 
 class DefaultUser(AbstractUser):
 
@@ -14,59 +12,55 @@ class DefaultUser(AbstractUser):
     def has_friend(self, possible_friend):
         return self.friendships.filter(friend=possible_friend).exists()
 
+class FriendshipQuerySet(models.QuerySet):
+
+    def create(self, user: DefaultUser, friend: DefaultUser):
+        default_fs = super().create(user=user, friend=friend)
+        reversed_fs = super().create(user=friend, friend=user)
+        default_fs.reversed = reversed_fs
+        reversed_fs.reversed = default_fs
+        default_fs.save()
+        reversed_fs.save()
+        return default_fs
 
 class Friendship(models.Model):
     user = models.ForeignKey(DefaultUser, on_delete=models.CASCADE, related_name='friendships')
     friend = models.ForeignKey(DefaultUser, on_delete=models.CASCADE)
+    reversed = models.ForeignKey('Friendship', on_delete=models.CASCADE, null=True, default=None)
     created_at = models.DateTimeField(auto_now_add=True)
+    objects = FriendshipQuerySet.as_manager()
 
     class Meta:
         unique_together = [['user', 'friend']]
 
+class FriendRequestManager(models.Manager):
+    def create(self, from_user: DefaultUser, to_user: DefaultUser):
+        friendship_queryset = from_user.friendships.filter(friend__username=to_user.username)
+        assert not friendship_queryset.exists(), AssertionError('User already friend in friend list.')
+        self.super().create(from_user=from_user, to_user=to_user)
 
 class FriendRequest(models.Model):
     from_user = models.ForeignKey(DefaultUser, on_delete=models.CASCADE, related_name='sent_friend_requests')
     to_user = models.ForeignKey(DefaultUser, on_delete=models.CASCADE, related_name='received_friend_requests')
     created_at = models.DateTimeField(auto_now_add=True)
+    objects = FriendRequestManager
 
     class Meta:
         unique_together = [['from_user', 'to_user']]
 
-    def accept_request(self):
+    def accept_request(self) -> Friendship:
         from_user = self.from_user
         to_user = self.to_user
+        friendship, _ = Friendship.objects.get_or_create(user=from_user, friend=to_user)
         self.delete()
-        Friendship.objects.get_or_create(user=from_user, friend=to_user)
+        return friendship
 
     def reject_request(self):
         self.delete()
 
-
 class Message(models.Model):
-    friendship = models.ForeignKey(Friendship, on_delete=models.CASCADE)
+    friendship = models.ForeignKey(Friendship, on_delete=models.CASCADE, related_name='messages')
     content = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     has_been_read = models.BooleanField(default=False)
     read_at = models.DateTimeField(null=True)
-
-
-@receiver(models.signals.post_delete, sender=Friendship)
-def remove_reversed_friendship(instance, **kwargs):
-    user = instance.user
-    friend = instance.friend
-    reversed_friendship = friend.friendships.filter(friend=user)
-    if reversed_friendship.exists():
-        reversed_friendship.delete()
-
-
-@receiver(models.signals.post_save, sender=Friendship)
-def create_reversed_friendship(instance, created, **kwargs):
-    if created:
-        user = instance.user
-        friend = instance.friend
-        reversed_friendship, _ = Friendship.objects.get_or_create(friend=user, user=friend)
-
-
-@receiver(models.signals.pre_save, sender=FriendRequest)
-def stop_request_to_friend(instance, **kwargs):
-    assert not instance.from_user.has_friend(instance.to_user), AssertionError('user_is_friend')
